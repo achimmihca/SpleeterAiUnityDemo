930e1d8f66d3cbe8ac8a1f4707975a65f80b92a8
Andreas Stange <anst.dev@gmail.com>
2025-08-04 08:58:55 +0200

    add DisposableStopwatch

---
+++ Assets/Scenes/DisposableStopwatch.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+/**
+ * A stopwatch that performs an action when it is disposed.
+ */
+public class DisposableStopwatch : IDisposable
+{
+    private static readonly List<string> placeholders = new() { "<millis>", "<ms>" };
+    private readonly Stopwatch stopwatch;
+    private readonly Action<Stopwatch> action;
+
+    public double ElapsedMilliseconds => stopwatch.ElapsedMilliseconds;
+    public TimeSpan Elapsed => stopwatch.Elapsed;
+    public long ElapsedTicks => stopwatch.ElapsedTicks;
+
+    public DisposableStopwatch(Action<Stopwatch> action)
+    {
+        this.action = action;
+        stopwatch = new Stopwatch();
+        stopwatch.Start();
+    }
+
+    // Logs the given text when the stopwatch is disposed.
+    // Thereby, the placeholder <millis> will be replaced with the elapsed milliseconds.
+    public DisposableStopwatch(string textWithPlaceholders, LogType logEventLevel = LogType.Log)
+        : this((sw) => LogTextWithPlaceholders(sw, textWithPlaceholders, logEventLevel))
+    {
+    }
+
+    public void Dispose()
+    {
+        stopwatch.Stop();
+        action.Invoke(stopwatch);
+    }
+
+    private static void LogTextWithPlaceholders(Stopwatch sw, string textWithPlaceholders, LogType logEventLevel)
+    {
+        string millis = sw.ElapsedMilliseconds.ToString();
+        string logText = textWithPlaceholders;
+        if (ContainsPlaceholder(logText))
+        {
+            foreach (string placeholder in placeholders)
+            {
+                logText = logText.Replace(placeholder, millis);
+            }
+        }
+        else
+        {
+            logText = $"{logText}: {millis} ms";
+        }
+
+        Debug.LogFormat(logEventLevel, LogOption.None, null, logText);
+    }
+
+    private static bool ContainsPlaceholder(string logText)
+    {
+        foreach (string placeholder in placeholders)
+        {
+            if (logText.Contains(placeholder))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
